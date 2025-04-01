e6e0439a509025b3cb0292ae40f07199d2c8c0be
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-04-01 13:11:27 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
+//menu scrip social links
+function toggleBar(event) {
+    const container = document.querySelector(".social-container");
+    const links = document.querySelector(".social-links");
+
+    // Toggle the expanded class on the container
+    container.classList.toggle("expanded");
+
+    // Show or hide social links
+    if (container.classList.contains("expanded")) {
+        links.style.display = "flex"; // Show the social links
+    } else {
+        links.style.display = "none"; // Hide the social links
+    }
+}
+
+// Close the container if the user clicks anywhere outside of it
+document.addEventListener('click', function(event) {
+    const container = document.querySelector(".social-container");
+    if (!container.contains(event.target)) {
+        container.classList.remove("expanded");
+        document.querySelector(".social-links").style.display = "none"; // Hide social links
+    }
+});
+
+
+
+//QR cod eapi script
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
